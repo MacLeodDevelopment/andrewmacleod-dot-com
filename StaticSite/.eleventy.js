32256d2c20df4777951417441ca5f9315a1bc222
MacLeod Development <andrewmacleod.com@gmail.com>
2025-11-20 19:16:57 +0000

    Convert to Nunjucks.
    Sort navigation.
    Sort ordering of content.

--- StaticSite/.eleventy.js
+++ StaticSite/.eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
     return {
         dir: {
             input: ".",
+            includes: "_includes",
             output: "_site"
         }
     };
--- StaticSite/experience/experience-003.md
+++ StaticSite/experience/experience-003.md
@@ -1,5 +1,6 @@
 ---
 title: Experience 3
+order: 2
 ---
 
 Here is some information about experience 3
